b679891506bf9e08eab40f1782b7aa44e9e529aa
TartaricAcid <baka943@qq.com>
2020-09-21 01:03:45 +0800

    1.15 version update

---
+++ src/main/java/com/github/tartaricacid/extraplayerrenderer/gui/GuiRenderConfig.java
@@ -0,0 +1,91 @@
+package com.github.tartaricacid.extraplayerrenderer.gui;
+
+import com.github.tartaricacid.extraplayerrenderer.config.ConfigFileManager;
+import com.github.tartaricacid.extraplayerrenderer.config.ConfigPOJO;
+import com.github.tartaricacid.extraplayerrenderer.event.RenderScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.lwjgl.glfw.GLFW;
+
+import static net.minecraftforge.fml.client.gui.GuiUtils.drawGradientRect;
+
+@OnlyIn(Dist.CLIENT)
+public class GuiRenderConfig extends Screen {
+    private static final float SCALE_MAX = 360f;
+    private static final float SCALE_MIN = 8f;
+
+    public GuiRenderConfig(ITextComponent titleIn) {
+        super(titleIn);
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float particleTick) {
+        super.render(mouseX, mouseY, particleTick);
+        int middleX = width / 2;
+        int middleY = height / 2;
+        drawGradientRect(0, middleX - 60, middleY - 45, middleX + 70, middleY + 45, 0xcc111111, 0xcc111111);
+        String text = I18n.format("gui.extra_player_renderer.config.text");
+        font.drawSplitString(text, middleX - 50, middleY - 35, 140, 0xffffffff);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scroll) {
+        if (scroll != 0) {
+            changeScaleValue((float) scroll * 3f);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        boolean result = false;
+        if (button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
+            RenderScreen.setYawOffset((float) deltaX + RenderScreen.getYawOffset());
+            result = true;
+        }
+
+        if (button == GLFW.GLFW_MOUSE_BUTTON_RIGHT) {
+            RenderScreen.setPosX((float) deltaX + RenderScreen.getPosX());
+            RenderScreen.setPosY((float) deltaY + RenderScreen.getPosY());
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean charTyped(char typedChar, int keyCode) {
+        if (Character.toLowerCase(typedChar) == 'r' && hasAltDown()) {
+            ConfigPOJO pojo = ConfigPOJO.getInstance();
+            RenderScreen.setPosX(pojo.getPosX());
+            RenderScreen.setPosY(pojo.getPosY());
+            RenderScreen.setScale(pojo.getScale());
+            RenderScreen.setYawOffset(pojo.getYawOffset());
+        }
+        return super.charTyped(typedChar, keyCode);
+    }
+
+    /**
+     * 更改 Scale 的数值
+     *
+     * @param amount 增加或者减少的数值
+     */
+    private void changeScaleValue(float amount) {
+        float tmp = RenderScreen.getScale() + amount * RenderScreen.getScale() / 80.0f;
+        RenderScreen.setScale(MathHelper.clamp(tmp, SCALE_MIN, SCALE_MAX));
+    }
+
+    @Override
+    public void onClose() {
+        ConfigFileManager.writeConfigFile(
+                new ConfigPOJO(RenderScreen.getPosX(),
+                        RenderScreen.getPosY(),
+                        RenderScreen.getScale(),
+                        RenderScreen.getYawOffset()));
+        super.onClose();
+    }
+}
---
+++ src/main/java/com/github/tartaricacid/extraplayerrenderer/input/OpenConfigKey.java
@@ -0,0 +1,33 @@
+package com.github.tartaricacid.extraplayerrenderer.input;
+
+import com.github.tartaricacid.extraplayerrenderer.gui.GuiRenderConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.lwjgl.glfw.GLFW;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
+public class OpenConfigKey {
+    public static final KeyBinding OPEN_CONFIG_KEY = new KeyBinding("key.extra_player_renderer.open_config.desc",
+            KeyConflictContext.IN_GAME,
+            KeyModifier.ALT,
+            InputMappings.Type.KEYSYM,
+            GLFW.GLFW_KEY_P,
+            "key.category.extra_player_renderer");
+
+    @SubscribeEvent
+    public static void onKeyboardInput(InputEvent.KeyInputEvent event) {
+        if (OPEN_CONFIG_KEY.isPressed()) {
+            Minecraft.getInstance().displayGuiScreen(new GuiRenderConfig(new StringTextComponent("config_gui")));
+        }
+    }
+}
