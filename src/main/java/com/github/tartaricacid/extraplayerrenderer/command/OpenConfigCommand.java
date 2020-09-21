f970fb213fa851362cce8bba9ac1883ce4cde3ce
TartaricAcid <baka943@qq.com>
2020-09-21 17:48:23 +0800

    1.10 version update

---
+++ src/main/java/com/github/tartaricacid/extraplayerrenderer/command/OpenConfigCommand.java
@@ -0,0 +1,37 @@
+package com.github.tartaricacid.extraplayerrenderer.command;
+
+import com.github.tartaricacid.extraplayerrenderer.gui.GuiRenderConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+
+public class OpenConfigCommand extends CommandBase {
+    private static final String NAME = "epr";
+    private static final String USAGE = "/epr";
+
+    @Override
+    public String getCommandName() {
+        return NAME;
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return USAGE;
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
+        new Thread(() -> {
+            try {
+                Minecraft.getMinecraft().addScheduledTask(() -> Minecraft.getMinecraft().displayGuiScreen(new GuiRenderConfig()));
+            } catch (Throwable e) {
+            }
+        }, "I18n_NOTICE_PENDING_THREAD").start();
+    }
+}
---
+++ src/main/java/com/github/tartaricacid/extraplayerrenderer/event/RenderScreen.java
@@ -0,0 +1,82 @@
+package com.github.tartaricacid.extraplayerrenderer.event;
+
+import com.github.tartaricacid.extraplayerrenderer.config.ConfigFileManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(value = Side.CLIENT)
+public class RenderScreen {
+    @SubscribeEvent
+    public static void onRenderScreen(RenderGameOverlayEvent event) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.HOTBAR) {
+            return;
+        }
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityPlayerSP player = mc.thePlayer;
+
+        GlStateManager.enableColorMaterial();
+        GlStateManager.enableDepth();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(ConfigFileManager.getConfigPojo().getPosX(), ConfigFileManager.getConfigPojo().getPosY(), -500.0F);
+        GlStateManager.scale(-ConfigFileManager.getConfigPojo().getScale(), ConfigFileManager.getConfigPojo().getScale(),
+                ConfigFileManager.getConfigPojo().getScale());
+        GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.rotate(player.rotationYaw + ConfigFileManager.getConfigPojo().getYawOffset(), 0.0F, 1.0F, 0.0F);
+
+        GlStateManager.rotate(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.rotate(-135.0F, 0.0F, 1.0F, 0.0F);
+
+        RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
+        rendermanager.setRenderShadow(false);
+        rendermanager.doRenderEntity(player, 0.0D, 0.0D, 0.0D, 0, event.getPartialTicks(), true);
+        rendermanager.setRenderShadow(true);
+
+        GlStateManager.popMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GlStateManager.disableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+    }
+
+    public static float getScale() {
+        return ConfigFileManager.getConfigPojo().getScale();
+    }
+
+    public static void setScale(float scale) {
+        ConfigFileManager.getConfigPojo().setScale(scale);
+    }
+
+    public static float getPosX() {
+        return ConfigFileManager.getConfigPojo().getPosX();
+    }
+
+    public static void setPosX(float posX) {
+        ConfigFileManager.getConfigPojo().setPosX(posX);
+    }
+
+    public static float getPosY() {
+        return ConfigFileManager.getConfigPojo().getPosY();
+    }
+
+    public static void setPosY(float posY) {
+        ConfigFileManager.getConfigPojo().setPosY(posY);
+    }
+
+    public static float getYawOffset() {
+        return ConfigFileManager.getConfigPojo().getYawOffset();
+    }
+
+    public static void setYawOffset(float yawOffset) {
+        ConfigFileManager.getConfigPojo().setYawOffset(yawOffset);
+    }
+}
